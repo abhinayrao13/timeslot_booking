b7e5a7e5ab2c9a63f95de23ac1eb0ed2d13d80d2
Abhinay Rao <abhinayrao13@gmail.com>
2017-09-10 11:20:55 +0530

    required moment jquery bootstap , bootstrap datepicker and fullcalendar in application js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require turbolinks
+//= require moment
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require bootstrap-datetimepicker
+//= require fullcalendar
 //= require_tree .
